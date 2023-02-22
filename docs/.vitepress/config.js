7433b7a061152a49ac1e94da17b23e85552f06b8
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2023-02-22 03:05:46 -0600

    Moved Google Analytics code

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,19 +2,19 @@ export default {
     title: 'Displagent Docs',
     description: 'The official docsite for Displagent.',
     cleanUrls: true,
+    head: [
+      [
+        'script',
+        { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-ZYJNTMM4SR' }
+      ],
+      [
+        'script',
+        {},
+        "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-ZYJNTMM4SR')"
+      ]
+    ],
     themeConfig: {
         logo: '/logo.png',
-        head: [
-          [
-            'script',
-            { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-ZYJNTMM4SR' }
-          ],
-          [
-            'script',
-            {},
-            "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-ZYJNTMM4SR')"
-          ]
-        ],
         nav: [
             { text: 'Setup', link: '/setup/' }
         ],
